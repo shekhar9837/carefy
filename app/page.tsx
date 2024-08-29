0467174a8110433ea0512355653f0455fe636492
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-08-29 16:51:33 +0530

    added homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,48 @@
-import { Button } from "@/components/ui/button";
-import { UserForm } from "@/components/UserForm";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { PatientForm } from "@/components/forms/PatientForm";
+
+const Home = () => {
+
   return (
-    <div className="text-5xl flex flex-row justify-between">
-      
-      <UserForm/>
+    <div className="flex h-screen max-h-screen">
 
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="flex gap-2 items-start justify-start ">
+          <Image
+            src="/assets/icons/logo-icon.svg"
+            height={1000}
+            width={1000}  
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+            />
+          <h1 className="text-[1.8rem] font-bold">CareFy</h1>
+            </div>
 
-      <Image src='/assets/images/onboarding-img.png' alt="onboarding-image" width={1000} height={1000}  className="w-[50%]"/>
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
-}
+};
+
+export default Home;
--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -17,38 +17,47 @@ import {
 import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  fullName: z.string().min(2, {
+    message: "Full name must be at least 2 characters.",
+  }),
+  email: z.string().email({ message: "Please enter a valid email address" }),
+
+  phoneNumber: z.string().min(10, {
+    message: "Phone number must be at least 10 digits long",
+  }).max(15, {
+    message: "Phone number must be no more than 15 digits long",
+  }).regex(/^\d+$/, {
+    message: "Phone number must contain only digits",
   }),
 })
 
 export function UserForm() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          username: "",
-        },
-      })
 
-      function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-      }
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      phoneNumber: ""
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}    className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="">
         <FormField
           control={form.control}
-          name="username"
+          name="fullName"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Full name</FormLabel>
               <FormControl>
-                <Input placeholder="Username" {...field} />
+                <Input placeholder="John Doe" {...field} />
               </FormControl>
-        
               <FormMessage />
             </FormItem>
           )}
@@ -58,26 +67,31 @@ export function UserForm() {
           name="email"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Email address</FormLabel>
               <FormControl>
-                <Input placeholder="Username" {...field} />
+                <Input placeholder="john@gmail.com" {...field} />
               </FormControl>
-              
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              
-              <FormLabel>email</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="phoneNumber"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone number</FormLabel>
+              <FormControl>
+                <Input placeholder="919191919191" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+                className= "shad-primary-btn w-full"
+
+        type="submit" >Submit</Button>
       </form>
     </Form>
   )
