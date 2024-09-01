d4e49c47dab7450388eac4d1d2b36491b04c13b9
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-09-01 21:32:14 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: false, 
+  }, eslint: {
+    ignoreDuringBuilds: true, 
+  },
+};
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
